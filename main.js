53d5e672e80007d53e1ae0ba5fb750dbb1231989
Kwon Dae-ho <101388919+vaaast-lake@users.noreply.github.com>
2022-12-12 13:36:41 +0900

    JS, CSS: navbar modified by ellie

--- main.js
+++ main.js
@@ -1,18 +1,20 @@
 'use strict';
 
+// Make navbar transarent when it is on the top.
 const navBar = document.querySelector('#navbar');
 
-const onSticky = () => {
-  let navTop = navBar.offsetTop;
+const onSticky = () => { 
+  // let navTop = navBar.offsetTop;
   let scrollPos = window.scrollY;
-
-  if (scrollPos > navTop) {
+  let navbarHeight = navBar.getBoundingClientRect().height;
+  
+  if (scrollPos > navbarHeight) {
     navBar.classList.add('sticky');
   } else {
     navBar.classList.remove('sticky');
   }
 }
 
-window.addEventListener('scroll', onSticky);
+document.addEventListener('scroll', onSticky);
 
 
