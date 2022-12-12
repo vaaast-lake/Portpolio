9a7c7fa171a75c1fcc65b1ec56c64f54a7acf283
Kwon Dae-ho <101388919+vaaast-lake@users.noreply.github.com>
2022-12-12 16:23:16 +0900

    JS, CSS: active border & scroll when click item by me

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const onSticky = () => {
   // let navTop = navBar.offsetTop;
   let scrollPos = window.scrollY;
   let navbarHeight = navBar.getBoundingClientRect().height;
-  
+
   if (scrollPos > navbarHeight) {
     navBar.classList.add('sticky');
   } else {
@@ -17,4 +17,25 @@ const onSticky = () => {
 
 document.addEventListener('scroll', onSticky);
 
+// Scroll to section when click item.
+const menu = document.querySelector('.navbar__menu');
 
+// create border line, scroll to section
+const activeItem = (event) => {
+  const target = event.target;
+  const activedItem = document.querySelector('.active');
+  const sectionString = target.textContent.toLowerCase();
+  const section = document.querySelector(`#${sectionString}`);
+  const sectionY = section.getBoundingClientRect().top;
+
+  activedItem.classList.remove('active');
+  target.classList.add('active');
+  window.scrollBy({
+    top: sectionY,
+    behavior: 'smooth'
+  });
+}
+
+menu.addEventListener('click', (event) => {
+  if (event.target.tagName === 'LI') activeItem(event);
+});
